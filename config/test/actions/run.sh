354a7be60556d110ee62b259762bd652138ab7fa
eneasj <eneasj@Ubuntu.myguest.virtualbox.org>
2023-03-12 21:35:49 -0300

    fix - testes

--- config/test/actions/run.sh
+++ config/test/actions/run.sh
@@ -5,7 +5,7 @@ python setup.py -q develop
 # mkdir -p "$PROJECT_DIR/datalake/test"
 # gsutil -m rsync -rn gs://datalake_template/* "$PROJECT_DIR/datalake/test"
 
-ENV=test py.test --cov=dextra.dna.myra_ink "${@:2}";
+ENV=test py.test --cov=med.room.* "${@:2}";
 exitcode=$?
 rm -f .coverage .coverage.*  # cleanup
 exit $exitcode
